c02f0821272134beb5f4dd9cd86b39f646ec62db
Rodrigo inTalk Staff <rcarvalho89@hotmail.com>
2024-04-18 01:22:52 -0300

    Copilot documentation and tests

--- src/main/java/com/aichallenge/SimilarityCheck.java
+++ src/main/java/com/aichallenge/SimilarityCheck.java
@@ -10,9 +10,18 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class SimilarityCheck 
-{
+/**
+ * This class provides methods for calculating cosine similarity between two vectors and vectorizing a string.
+ */
+public class SimilarityCheck {
 
+    /**
+     * Calculates the cosine similarity between two vectors.
+     *
+     * @param vector1 the first vector represented as a map of terms and their frequencies
+     * @param vector2 the second vector represented as a map of terms and their frequencies
+     * @return the cosine similarity between the two vectors
+     */
     public static double cosineSimilarity(Map<String, Integer> vector1, Map<String, Integer> vector2) {
         Set<String> both = new HashSet<>(vector1.keySet());
         both.retainAll(vector2.keySet());
@@ -30,16 +39,20 @@ public class SimilarityCheck
         return dotProduct / (Math.sqrt(magnitude1) * Math.sqrt(magnitude2));
     }
 
-
-    public static Map<String, Integer> vectorizeString(String inputString) throws IOException 
-    {
+    /**
+     * Vectorizes a given string by counting the frequency of each term.
+     *
+     * @param inputString the input string to be vectorized
+     * @return a map of terms and their frequencies in the input string
+     * @throws IOException if an I/O error occurs while reading the input string
+     */
+    public static Map<String, Integer> vectorizeString(String inputString) throws IOException {
         Map<String, Integer> wordCount = new HashMap<>();
         ClassicTokenizer tokenizer = new ClassicTokenizer();
         tokenizer.setReader(new StringReader(inputString));
         tokenizer.reset();
 
-        while (tokenizer.incrementToken()) 
-        {
+        while (tokenizer.incrementToken()) {
             String token = tokenizer.getAttribute(CharTermAttribute.class).toString();
             wordCount.put(token, wordCount.getOrDefault(token, 0) + 1);
         }
@@ -50,9 +63,14 @@ public class SimilarityCheck
         return wordCount;
     }
 
-    public static void main( String[] args ) throws IOException
-    {
+    /**
+     * Main method to demonstrate the usage of the SimilarityCheck class.
+     *
+     * @param args command line arguments (not used)
+     * @throws IOException if an I/O error occurs while vectorizing the input strings
+     */
+    public static void main(String[] args) throws IOException {
         System.out.println(vectorizeString("Hello AI Challenging World!"));
-        System.out.println(cosineSimilarity(vectorizeString("Hello AI Challenging World!"),vectorizeString("The world is being challenged by AI!") ));
+        System.out.println(cosineSimilarity(vectorizeString("Hello AI Challenging World!"), vectorizeString("The world is being challenged by AI!")));
     }
 }
--- src/test/java/com/aichallenge/SimilarityCheckTest.java
+++ src/test/java/com/aichallenge/SimilarityCheckTest.java
@@ -1,20 +1,49 @@
 package com.aichallenge;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class SimilarityCheckTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimilarityCheckTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testCosineSimilarity() {
+        Map<String, Integer> vector1 = new HashMap<>();
+        vector1.put("Hello", 1);
+        vector1.put("AI", 1);
+        vector1.put("Challenging", 1);
+        vector1.put("World!", 1);
+
+        Map<String, Integer> vector2 = new HashMap<>();
+        vector2.put("The", 1);
+        vector2.put("world", 1);
+        vector2.put("is", 1);
+        vector2.put("being", 1);
+        vector2.put("challenged", 1);
+        vector2.put("by", 1);
+        vector2.put("AI!", 1);
+
+        double expectedSimilarity = 0.5;
+        double actualSimilarity = SimilarityCheck.cosineSimilarity(vector1, vector2);
+
+        assertEquals(expectedSimilarity, actualSimilarity, 0.0001);
+    }
+
+    @Test
+    public void testVectorizeString() throws IOException {
+        String inputString = "Hello AI Challenging World!";
+        Map<String, Integer> expectedVector = new HashMap<>();
+        expectedVector.put("Hello", 1);
+        expectedVector.put("AI", 1);
+        expectedVector.put("Challenging", 1);
+        expectedVector.put("World!", 1);
+
+        Map<String, Integer> actualVector = SimilarityCheck.vectorizeString(inputString);
+
+        assertEquals(expectedVector, actualVector);
     }
 }
