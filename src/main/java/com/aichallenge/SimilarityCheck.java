31c59f4f605328e236e0ed056b294b7a1b99838e
Rodrigo inTalk Staff <rcarvalho89@hotmail.com>
2024-04-17 21:55:47 -0300

    new commit

--- src/main/java/com/aichallenge/SimilarityCheck.java
+++ src/main/java/com/aichallenge/SimilarityCheck.java
@@ -9,7 +9,5 @@ public class SimilarityCheck
     public static void main( String[] args )
     {
         System.out.println("Hello AI Challenging World!" );
-        System.out.println("This is a test message.");
-        System.out.println("step 2");
     }
 }
